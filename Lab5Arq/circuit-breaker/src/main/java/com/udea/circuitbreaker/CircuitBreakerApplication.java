fa7f1987064135e452559d57557d10f227915b58
jersonlopez <jersonlopez.1204@gmail.com>
2017-11-15 14:54:32 -0500

    practica 5 terminada (clase)

---
+++ Lab5Arq/circuit-breaker/src/main/java/com/udea/circuitbreaker/CircuitBreakerApplication.java
@@ -0,0 +1,14 @@
+package com.udea.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class CircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerApplication.class, args);
+	}
+}
--- Lab5Arq/microservice-registration/src/main/java/com/udea/microserviceregistration/MicroserviceRegistrationApplication.java
+++ Lab5Arq/microservice-registration/src/main/java/com/udea/microserviceregistration/MicroserviceRegistrationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class MicroserviceRegistrationApplication {
 
 	public static void main(String[] args) {
